83fa4bf9c2f34dda726ac42766eb608b46aa598f
Giuseppe <giuseppe.sardo@clearscore.com>
2023-03-26 19:22:12 +1100

    Initial Commit

---
+++ AroundTheWorld/AroundTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundTheWorldApp.swift
+//  AroundTheWorld
+//
+//  Created by Giuseppe Sardo on 26/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AroundTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
